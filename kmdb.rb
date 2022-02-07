5b6eab1ef418acd30e47c60a0c0ac74e8b6c1440
Davidr1002 <david.rau@kellogg.northwestern.edu>
2022-02-07 19:57:56 +0000

    progress1

--- kmdb.rb
+++ kmdb.rb
@@ -68,14 +68,85 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+Person.destroy_all
+Movie.destroy_all
+Role.destroy_all
+
 
 # Generate models and tables, according to the domain model
-# TODO!
+
 
 # Insert data into your database that reflects the sample data shown above
 # Do not use hard-coded foreign key IDs.
-# TODO!
+christopher_nolan = Person.new
+christopher_nolan.name = "Christopher Nolan"
+christopher_nolan.save
+
+christian_bale = Person.new
+christian_bale.name = "Christian Bale"
+christian_bale.save
+
+michael_caine = Person.new
+michael_caine.name = "Michael Caine"
+michael_caine.save
+
+liam_neeson = Person.new
+liam_neeson.name = "Liam Neeson"
+liam_neeson.save
+
+katie_holmes = Person.new
+katie_holmes.name = "Katie Holmes"
+katie_holmes.save
+
+gary_oldman = Person.new
+gary_oldman.name = "Gary Oldman"
+gary_oldman.save
+
+heath_ledger = Person.new
+heath_ledger.name = "Heath Ledger"
+heath_ledger.save
+
+aaron_eckhart = Person.new
+aaron_eckhart.name = "Aaron Eckhart"
+aaron_eckhart.save
+
+maggie_gyllenhaal = Person.new
+maggie_gyllenhaal.name = "Maggie Gyllenhaal"
+maggie_gyllenhaal.save
+
+tom_hardy = Person.new
+tom_hardy.name = "Tom Hardy"
+tom_hardy.save
+
+joseph_gordon_levitt = Person.new
+joseph_gordon_levitt.name = "Joseph Gordon-Levitt"
+joseph_gordon_levitt.save
+
+anna_hathaway = Person.new
+anna_hathaway.name = "Anne Hathaway"
+anna_hathaway.save
+
+
+batman_begins = Movie.new
+batman_begins.title = "Batman Begins"
+batman_begins.year_released = "2005"
+batman_begins.rated = "PG-13"
+batman_begins.person_id = christopher_nolan.id
+batman_begins.save
+
+the_dark_knight = Movie.new
+the_dark_knight.title = "The Dark Knight"
+the_dark_knight.year_released = "2008"
+the_dark_knight.rated = "PG-13"
+the_dark_knight.person_id = christopher_nolan.id
+the_dark_knight.save
+
+the_dark_knight_rises = Movie.new
+the_dark_knight_rises.title = "The Dark Knight Rises"
+the_dark_knight_rises.year_released = "2012"
+the_dark_knight_rises.rated = "PG-13"
+the_dark_knight_rises.person_id = christopher_nolan.id
+the_dark_knight_rises.save
 
 # Prints a header for the movies output
 puts "Movies"
@@ -83,7 +154,11 @@ puts "======"
 puts ""
 
 # Query the movies data and loop through the results to display the movies output
-# TODO!
+movies = Movie.all
+for movie in movies
+    director = Person.where({id: movie.person_id})[0]
+    puts "#{movie.title} #{movie.year_released} #{movie.rated} #{director.name}"
+end
 
 # Prints a header for the cast output
 puts ""
@@ -92,4 +167,9 @@ puts "========"
 puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie
-# TODO!
+#roles = Role.all
+# for role in roles
+ #   movie = Movie.where({id: role.movie_id})[0] 
+ #   person = Person.where({id: role.person_id})[0]
+ #  puts "#{movie.title} #{person.name} #{role.character_name}"
+#end
